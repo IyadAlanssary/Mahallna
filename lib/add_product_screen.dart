595ca6260eabba723a47b1198972a3c76b73160c
Mohammad Hamed <67291298+Hamsho@users.noreply.github.com>
2022-01-07 08:55:04 +0200

    Update add_product_screen.dart

--- lib/add_product_screen.dart
+++ lib/add_product_screen.dart
@@ -6,6 +6,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:products/common widgets/image_local.dart';
 import 'package:products/common widgets/new_product_txt_field.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 import 'dart:io';
 
 class AddProduct extends StatefulWidget {
